284e333ccc441c2bfd59e210ef22428b409bd7b6
Dmitry Dorofeev <me@dmitrydorofeev.ru>
2015-10-31 11:01:06 +0300

    fix

--- src/android/VkSdk.java
+++ src/android/VkSdk.java
@@ -22,7 +22,6 @@ import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-@SuppressLint("SetJavaScriptEnabled")
 public class VkSdk extends CordovaPlugin {
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
         if (action.equals("greet")) {
